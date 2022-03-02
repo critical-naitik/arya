f9e7165a3cc2035ebe7da08dbc6158a5133338a0
critical-naitik <critical1710@gmail.com>
2022-03-03 03:28:22 +0530

    Colors declared

---
+++ utils/colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appName = "Arya-mates";
+
+  //Colors for theme
+  static Color lightPrimary = Color(0xfffcfcff);
+  static Color red = Color(0xfff62f2f);
+
+  static Color? lightAccent = Colors.blueGrey[900];
+  static Color darkAccent = Colors.white;
+
+  static Color lightBG = Color(0xfffcfcff);
+  static Color textcolor = Colors.black;
+  static Color badgeColor = Colors.red;
+  static Color Orange = Color(0xffee9503);
+
+  // theme colors are defined below :
+
+  // for light theme
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: Orange,
+    primaryColor: lightPrimary,
+    accentColor: lightAccent,
+    cursorColor: lightAccent,
+    scaffoldBackgroundColor: lightBG,
+
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          color: textcolor,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+
+
+  //for Dark Theme
+
+
+}
